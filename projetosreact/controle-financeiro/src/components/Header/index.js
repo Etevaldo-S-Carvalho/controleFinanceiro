f5a24bc1e9d51cabe995afc2eb953e5b2aeeae90
etevaldo2019 <silva_etevaldo@hotmail.com>
2024-09-28 21:26:43 -0300

    subirProjeto
    meu primeiro commit

---
+++ projetosreact/controle-financeiro/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as C from './styles';
+
+const Header = () =>{
+  return (
+    <C.Container>
+      <C.Header>
+        <C.Title>Controle Financeiro Mês de Setembro</C.Title>
+      </C.Header>
+    </C.Container>
+  );
+}
+
+export default Header
